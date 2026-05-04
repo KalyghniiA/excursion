efd3a3872f2fa815f4119ea43fd47fcbddf3f17e
Александр <kalyghnii@gmail.com>
2026-05-04 18:15:06 +0500

    poster

---
+++ src/interfaces/Affiche.interface.ts
@@ -0,0 +1,10 @@
+export interface Affiche {
+    id: string;
+    name: string;
+    promo: string;
+    date: string;
+    genre: string;
+    posterUrl: string;
+    start?: string;
+    rating?: string;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import {imageToWebpPlugin} from "vite-plugin-image-to-webp";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()]
+	plugins: [
+		react(),
+		imageToWebpPlugin({
+			imageFormats: ["jpg", "jpeg", "png"],
+			webpQuality: {},
+			destinationFolder: "dist"
+		})
+	]
 });
